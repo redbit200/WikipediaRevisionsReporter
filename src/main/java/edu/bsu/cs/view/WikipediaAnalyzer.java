b0674ad74faa1fe59da0fdf4a4ce156ed8cc5f9c
Collin <ecstiner@bsu.edu>
2021-04-26 14:08:38 -0400

    Complete challenge 3

--- src/main/java/edu/bsu/cs/view/WikipediaAnalyzer.java
+++ src/main/java/edu/bsu/cs/view/WikipediaAnalyzer.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
 
 public final class WikipediaAnalyzer extends VBox {
 
@@ -62,13 +63,7 @@ public final class WikipediaAnalyzer extends VBox {
     private void runQuery(String articleTitle) {
         try {
             QueryResponse response = engine.queryRevisions(articleTitle);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (Revision revision : response.revisions()) {
-                String message = formatter.format(revision);
-                stringBuilder.append(message);
-                stringBuilder.append("\n");
-            }
-            outputArea.setText(stringBuilder.toString());
+            outputArea.setText(response.revisions().stream().map(revision -> formatter.format(revision)).collect(Collectors.joining("\n")));
         } catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Connection Problem");
